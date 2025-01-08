b86723b8bbfb5d1a5b189269154c2ebc28bbacbb
William Monteiro <williamfm64@gmail.com>
2025-01-08 19:24:39 -0300

    Criados os métodos necessários para cadastrar os colaboradores no banco de dados MongoDB

---
+++ server.js
@@ -0,0 +1,55 @@
+import express from 'express'
+
+import { PrismaClient } from '@prisma/client'
+
+const app = express()
+
+const prisma = new PrismaClient()
+
+app.use(express.json())
+
+app.post('/coworkers', async (req, res) => {
+    await prisma.coworker.create({
+        data : {
+            name: req.body.name,
+            role: req.body.role,
+            image: req.body.image,
+            team: req.body.team
+        }
+    })
+    res.status(201).json(req.body)
+})
+
+app.get('/coworkers', async (req, res) =>{
+
+    const data = await prisma.coworker.findMany()
+
+    res.status(200).json(data)
+
+})
+
+app.patch('/coworkers/:id', async (req, res) => {
+    await prisma.coworker.update({
+        where : {
+            id : req.params.id
+        },
+        data : {
+            name: req.body.name,
+            role: req.body.role,
+            image: req.body.image,
+            team: req.body.team
+        }
+    })
+    res.status(201).json(req.body)
+})
+
+app.delete('/coworkers/:id', async (req, res) => {
+    await prisma.coworker.delete({
+        where : {
+            id : req.params.id
+        }
+    })
+    res.status(200).json({message: "Usuário foi deletado com sucesso."})
+})
+
+app.listen(3001)
